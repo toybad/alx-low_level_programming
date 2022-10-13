2f9b4c142daa1ca97320753715bfae03443a0904
toybad <58037053+toybad@users.noreply.github.com>
2022-10-13 16:50:04 +0100

    Update 5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
 /**
- * main - Entry
- * Return: Always 0
+ * main - Entry point
+ *
+ * Return: 0
  */
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,\n);
-	return (0)
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
 }
